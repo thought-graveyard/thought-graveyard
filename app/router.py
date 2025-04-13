62fc5c645ea4e21fa90ee3e5e0a5cb40503ab759
jinseok <newyoplait@gmail.com>
2025-04-13 21:01:46 +0800

    Start implementing login page and route

--- app/router.py
+++ app/router.py
@@ -1,7 +1,12 @@
 # File to store routes of flask app
 
 from flask import render_template
+from app import app  # 이 줄이 필요합니다
 
 @app.route('/')
 def index():
     return render_template('landing_page.html')
+
+@app.route('/login')
+def login():
+    return render_template('login_page.html')
