e941b9bbd6f1a7ccd139e8357ac9b5974bfe7be9
jinseok <newyoplait@gmail.com>
2025-04-18 14:14:05 +0800

    Update router.py to fix missing template issue

--- app/router.py
+++ app/router.py
@@ -1,11 +1,10 @@
-# File to store routes of flask app
 
-from flask import render_template
+from flask import render_template, redirect
 from app import app 
 
 @app.route('/')
 def index():
-    return render_template('landing_page.html')
+    return redirect('/register')
 
 @app.route('/login')
 def login():
