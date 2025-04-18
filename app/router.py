35be793f401f8e90b4b76c646d71200bf59ebf41
jinseok <newyoplait@gmail.com>
2025-04-18 14:11:03 +0800

    Update router.py with register route from main branch

--- app/router.py
+++ app/router.py
@@ -10,3 +10,7 @@ def index():
 @app.route('/login')
 def login():
     return render_template('login_page.html')
+
+@app.route('/register')
+def register():
+    return render_template('Registerform.html')
